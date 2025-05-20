5a2d1ce1aa9ab12053e236fce5156e399514c570
Jap <97182832+JaProCar@users.noreply.github.com>
2025-05-20 14:19:11 +0300

    Update head.h

--- head.h
+++ head.h
@@ -1,34 +1,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 namespace PACK{
 class post {
-    std::string _name;
-    int index;
-    int _x; // Координата X
-    int _y; // Координата Y
-    std::vector<int> _packs; // Посылки в отделении
 public:
-    post() = default;
-    post(int index, int x, int y, std::vector<int> packs);
+    int name;
+    int x; // Координата X
+    int y; // Координата Y
+    vector<int> _packs; // Посылки в отделении
+};
+class pack{
+public:
+    int _id;               // трек-номер
+    string _sendname; // имя отправителя
+    string _getname;  // имя получателя
+    int _to;               // приемное отделение
+    int _weight;           // вес посылки
+    int _remainingTime;    // оставшееся время доставки в днях
+    int _currentX;         // x сейчас
+    int _currentY;         // y сейчас
 
+    
+};
     void display();
     void addpost();
     void delpost();
     double distance();
-};
-class pack{
-    std::string _sendname; // имя отправителя
-    std::string _getname;  // имя получателя
-    int _from;             // отправочное отделение
-    int _to;               // приемное отделение
-    int _weight;           // вес посылки
-    int _id;               // трек-номер
-public:
-    int _remainingTime;    // оставшееся время доставки в днях
-    int _currentX;         // x сейчас
-    int _currentY;         // y сейчас
     void add();
-    
-};
 }
