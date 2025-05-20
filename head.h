cab3c1915625b939ca8d1b4cecd52e5b53055307
Jap <97182832+JaProCar@users.noreply.github.com>
2025-05-20 15:27:51 +0300

    Update head.h

--- head.h
+++ head.h
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
+
 namespace PACK{
 class post {
 public:
@@ -9,17 +11,19 @@ public:
     int x; // Координата X
     int y; // Координата Y
     vector<int> _packs; // Посылки в отделении
+    friend ostream& operator<<(ostream& os, const post& Post);
+    friend istream& operator>>(istream& is, const post& Post);
 };
 class pack{
 public:
-    int _id;               // трек-номер
-    string _sendname; // имя отправителя
-    string _getname;  // имя получателя
-    int _to;               // приемное отделение
-    int _weight;           // вес посылки
-    int _remainingTime;    // оставшееся время доставки в днях
-    int _currentX;         // x сейчас
-    int _currentY;         // y сейчас
+    int id;               // трек-номер
+    string sendname; // имя отправителя
+    string getname;  // имя получателя
+    int to;               // приемное отделение
+    int weight;           // вес посылки
+    int remainingTime;    // оставшееся время доставки в днях
+    int currentX;         // x сейчас
+    int currentY;         // y сейчас
     friend ostream& operator<<(ostream& os, const pack& Pack);
     friend istream& operator>>(istream& is, const pack& Pack);
 };
@@ -29,3 +33,4 @@ public:
     double distance();
     void add();
 }
+
